d496ff3c52eea2917e006ceca6d56e5ff84d4fed
Dustin Gouner <dustingouner20@gmail.com>
2022-11-09 15:35:50 -0700

    Add an else if statement

--- friends.js
+++ friends.js
@@ -1,8 +1,14 @@
-var friend = "John"
+var friend = "Steve"
 
 if (friend === "Mike") {
     console.log("When is the next time we will grab a beer?")
 } 
+
+else if (friend === "Garret"){
+    console.log("How's the family?")
+}
+
+
 else{
     console.log(`"Long time no see, ${friend}"`)
 
